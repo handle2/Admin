434e9df951ce261afaa489c1ef893717ca17f844
unyikrisz <krisztian@chrome.hu>
2016-12-22 20:23:10 +0100

    prodcategs,products,inputs

--- config/config.php
+++ config/config.php
@@ -1,12 +1,4 @@
 <?php
-function dd($data,$die = true){
-    echo "<pre>";
-        var_dump($data);
-    echo "</pre>";
-    if($die){
-        die();
-    }
-}
 
 return new \Phalcon\Config(array(
     'database' => array(
